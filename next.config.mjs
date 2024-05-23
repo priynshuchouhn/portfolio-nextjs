bbdac55820f2e8300a8ecc607ee3ca8ddb7ac820
Priyanshu Chouhan <priyanschouhan123@gmail.com>
2024-05-23 22:13:07 +0530

    Minor fixes

--- next.config.mjs
+++ next.config.mjs
@@ -6,8 +6,8 @@ export default withSentryConfig(nextConfig, {
 // For all available options, see:
 // https://github.com/getsentry/sentry-webpack-plugin#options
 
-org: "square-code-technologies",
-project: "javascript-nextjs",
+org: "priynshuchouhn",
+project: "portfolio",
 
 // Only print logs for uploading source maps in CI
 silent: !process.env.CI,
